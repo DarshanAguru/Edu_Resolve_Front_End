97ce3b181f2301e0a4c4ba4aff713d6a6a36e5e1
Sai Raghava Ithireddy <sairaghava032@gmail.com>
2024-03-28 23:54:19 +0530

    Update ViewQuestions.jsx

--- src/assets/Pages/Student/ViewQuestions.jsx
+++ src/assets/Pages/Student/ViewQuestions.jsx
@@ -1,33 +1,48 @@
 import React from "react";
 
 const viewQuestions = ({ questions }) => {
+  console.log(questions);
+  const { AssignmentData, studentData } = questions;
   return (
-    <div className=" m-5 p-5 border">
-      {console.log(questions)}
-      <p className=" font-semibold">
-        <span>1.{"  "}</span>What is 2+2 ?
-      </p>
+    <>
+      {Object.entries(questions).length > 0 && (
+        <div className=" m-5 p-5 border">
+          {AssignmentData.map((assignment, index) => {
+            return (
+              <div>
+                <p className=" font-semibold">
+                  <span className=" font-medium tracking-wide">
+                    {index + 1}
+                    {")  "}
+                  </span>
+                  {assignment.text}
+                </p>
 
-      <div className="flex gap-2 mb-2">
-        <p className=" font-semibold">Options: </p>
-        <p className="">a 4</p>
-        <p className="">b four</p>
-        <p className="">c 4</p>
-        <p className="">d 4</p>
-      </div>
-      <div className="flex gap-10">
-        <div>
-          <p className=" font-semibold">Correct Answers</p>
-          <p>4</p>
-          <p>four</p>
+                <div className="flex gap-2 mb-2">
+                  {assignment.options.map((option, index) => {
+                    return (
+                      <p className="">{`${String.fromCharCode(index + 97)}) ${option.value}`}</p>
+                    );
+                  })}
+                </div>
+                {/* <div className="flex gap-10">
+                  <div>
+                    <p className=" font-semibold">Correct Answers</p>
+                    <p>4</p>
+                    <p>four</p>
+                  </div>
+                  <div>
+                    <p className=" font-semibold">Your answers</p>
+                    <p>4</p>
+                    <p>four</p>
+                  </div>
+                </div> */}
+              </div>
+            );
+          })}
         </div>
-        <div>
-          <p className=" font-semibold">Your answers</p>
-          <p>4</p>
-          <p>four</p>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
